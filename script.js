434fc0e8517437028895c7cd94a5a19b9d338e13
Vlad <vlad.kuzmichw@gmail.com>
2019-03-27 22:37:14 +0300

    try typeof

--- script.js
+++ script.js
@@ -3,11 +3,17 @@ var int = 5                 //number
 var nu = null               //null
 var un                      //unknow
 var bool = true             //boolean
-var ob = {name = "Vlad"}    //object
+var ob = {name: "Vlad"}    //object
 
 
 console.log(typeof str)
 console.log(typeof int)
+
+console.log(Number.MAX_VALUE)
+console.log(Number.MIN_VALUE)
+console.log(str+int)
+console.log(int+str)
+
 console.log(typeof nu)
 console.log(typeof un)
 console.log(typeof bool)
