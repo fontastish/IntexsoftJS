0ac55fef2e6a2a4f90802031bca14443f3073afa
Vlad <vlad.kuzmichw@gmail.com>
2019-03-27 22:25:19 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var str = "Hello"           //string
+var int = 5                 //number
+var nu = null               //null
+var un                      //unknow
+var bool = true             //boolean
+var ob = {name = "Vlad"}    //object
+
+
+console.log(typeof str)
+console.log(typeof int)
+console.log(typeof nu)
+console.log(typeof un)
+console.log(typeof bool)
+console.log(typeof ob)
