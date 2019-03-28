7f1130d7d197a13dd255fddbaa1a03fe5748cd19
Vlad <vlad.kuzmichw@gmail.com>
2019-03-28 11:56:59 +0300

    one var

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ var int = 5;                 //number
 var nu = null;               //null
 var un;                      //unknow
 var bool = true;             //boolean
-var ob = {name: "Vlad"};     //object
+var ob = {
+    name: "Vlad"
+};     //object
 
 
 console.log(typeof str);
@@ -18,3 +20,12 @@ console.log(typeof nu);
 console.log(typeof un);
 console.log(typeof bool);
 console.log(typeof ob);
+
+var str = "Hello",           //string
+    int = 5,                 //number
+    nu = null,               //null
+    un,                      //unknow
+    bool = true,             //boolean  
+    ob = {
+    name: "Vlad"
+};     //object
