6b9a70270c07efd6f1dcd9ca8fcd0a2c15f753d4
Vlad <vlad.kuzmichw@gmail.com>
2019-04-03 11:23:35 +0300

    Complete second task

--- Lecture_3/script.js
+++ Lecture_3/script.js
@@ -1,14 +1,34 @@
 var h = 26,
     m = 35;
 
-function isTimeValid(a,b){
+function isTimeValid(a, b){                               //first task
     if(typeof(a)=='number' && typeof(b) =='number' ){
-        if (a>=1 && a<=24 && b >= 0 && b<=60){
+        if (a>=1 && a<=24 && b >= 0 && b<=59){
             return true;
         } else{
             return false;
         }    
     }else {
+        return false;
+    }
+}
+
+function addMinutes(h, m, addM){
+    if(typeof(h)=='number' && typeof(m) =='number' && typeof(addM) == 'number' ){
+        if(isTimeValid(h, m) == true){
+            m+=addM;
+            if (m>=60){
+                h+=Math.floor(m/60);
+                m%=60;
+            }
+            if(h>24){
+                h%=24;
+            }
+            return h + ':' + m;
+        } else{
+            return false;
+        }
+    } else {
         return false;
     }
 }
