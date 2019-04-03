282c63fa8ad87a9faebd39e78a0eecb72bdbca5e
Vlad <vlad.kuzmichw@gmail.com>
2019-04-03 10:39:51 +0300

    Add hierarchy

---
+++ Lecture_1/script.js
@@ -0,0 +1,21 @@
+var str = "Hello",           //string
+    int = 5,                 //number
+    nu = null,               //null
+    un,                      //unknow
+    bool = true,             //boolean  
+    ob = {
+    name: "Vlad"
+};     //object
+
+console.log(typeof str);
+console.log(typeof int);
+
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+console.log(str+int);
+console.log(int+str);
+
+console.log(typeof nu);
+console.log(typeof un);
+console.log(typeof bool);
+console.log(typeof ob);
