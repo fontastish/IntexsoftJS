59a7ac4b5274f531fe4d88b4d4bfbbbcd201c39a
Vlad <vlad.kuzmichw@gmail.com>
2019-04-13 14:01:56 +0300

    start

---
+++ Lecture_5/solution.js
@@ -0,0 +1,10 @@
+//Подсказка: сюда можно складывать записи адресной книги.
+var phoneBook = [];
+
+//Здесь можно объявить переменные и функции, которые понядобятся вам для работы ваших функций
+
+module.exports = {
+    getWords: function(sentence){},
+    normalizeWords: function(words){},
+    addressBook: function(command){}
+}
