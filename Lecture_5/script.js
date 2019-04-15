55a3e6ffa063d1b2cfb4c9f441c4755648ca597d
Vlad <vlad.kuzmichw@gmail.com>
2019-04-15 13:47:25 +0300

    fix addressBook

--- Lecture_5/script.js
+++ Lecture_5/script.js
@@ -35,16 +35,16 @@ function addressBook(str) {
     var tempArr = str.split(/[ |,]/);
     switch (tempArr[0]) {
         case 'ADD':
-            if (!phoneBook.some(function (item) {
-                return item.split(":")[0]
+            if (!phoneBook.some(function (item) {           //Проверка наличие слова
+                    return item.split(":")[0]
                     .localeCompare(tempArr[1]) === 0
-            })) {
+            })) {                                                       //Если нету записи, добавляем
                 finalReturn = phoneBook.push(tempArr[1] + ': '
-                    + tempArr.slice(2, tempArr.length).join());
-            } else {
+                    + tempArr.slice(2, tempArr.length).join(', '));
+            } else {                                                    //Есть запись
                 phoneBook = phoneBook.map(function (item) {
                     return item.split(/: |,/)[0].localeCompare(tempArr[1]) === 0
-                        ? item.concat(',' + tempArr.slice(2, tempArr.length))
+                        ? item.concat(', ' + tempArr.slice(2, tempArr.length))
                         : item;
                 })
                 finalReturn = phoneBook.length;
@@ -54,18 +54,18 @@ function addressBook(str) {
             finalReturn = phoneBook.sort();
             break;
         case 'REMOVE_PHONE':
-            if (phoneBook.some(function (item) {
-                return item.split(/: |,/)
+            if (phoneBook.some(function (item) {                //Определяем есть ли такой номер
+                return item.split(/ |, /g)
                     .indexOf(tempArr[1]) !== -1
             })) {
-                phoneBook = phoneBook.map(function (eachItem) {
-                    eachItem = eachItem.split(/: |,/).filter(function (item) {
+                phoneBook = phoneBook.map(function (eachItem) {         // Мап для изменения записи
+                    eachItem = eachItem.split(/ |, /g).filter(function (item) {   //Фильтр чтобы удалить нужный номер из записи
                         return item.localeCompare(tempArr[1]) !== 0;
                     });
-                    return eachItem[0] + ': ' + eachItem.slice(1, eachItem.length);
+                    return eachItem[0] + ' ' + eachItem.slice(1, eachItem.length).join(', ');
                 });
-                phoneBook = phoneBook.filter(function (item) {
-                    return item.split(/: |,|:/)[1].localeCompare('')
+                phoneBook = phoneBook.filter(function (item) {      // Перепроверка всех контактов на наличие в них телефонов
+                    return item.split(/: |,|:/).length > 1;
                 });
                 finalReturn = true;
             } else {
@@ -79,5 +79,6 @@ function addressBook(str) {
 addressBook('ADD Vlad 00000');
 addressBook('ADD Ivan 33-33-33,45-456-5');
 addressBook('ADD Avan 100000,99999');
-addressBook('SHOW');
+addressBook('ADD Vlad 444');
+console.log(addressBook('SHOW'));
 //addressBook('REMOVE_PHONE 99999');
